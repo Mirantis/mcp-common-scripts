3e1101ec0ee55d51b62b5e1ccb9d43c5c4a52ded
Richard Felkl <rfelkl@mirantis.com>
2017-11-28 08:45:21 +0100

    changed images location

--- mirror-image/images_mirror/update-images.sh
+++ mirror-image/images_mirror/update-images.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 FILES="$(cat /srv/images.txt)"
+URL="http://images.mirantis.com"
+while getopts "u:"  option
+do
+ case "${option}"
+ in
+ u|\?) URL=${OPTARG};;
+ esac
+done
+
 for FILE in $FILES
 do
   FILENAME=`echo $FILE | sed 's/.*\///g'`
@@ -10,7 +19,7 @@ do
       MD5="None"
       echo "===> File /srv/http/images/$FILENAME doesn't exist"
   fi
-  wget http://apt.mirantis.com/images/$FILENAME.md5 -q -O /srv/http/images/$FILENAME.md5
+  wget $URL/$FILENAME.md5 -q -O /srv/http/images/$FILENAME.md5
   MD5UPSTREAM=`cat /srv/http/images/$FILENAME.md5 | awk '{ print $1 }'`
   rm /srv/http/images/$FILENAME.md5
   if [ "$MD5" != "$MD5UPSTREAM" ];
@@ -19,7 +28,7 @@ do
       echo "Local MD5 hash is:    $MD5"
       echo "Upstream MD5 hash is: $MD5UPSTREAM"
       rm /srv/http/images/$FILENAME
-      wget http://apt.mirantis.com/images/$FILENAME -O /srv/http/images/$FILENAME
+      wget $URL/$FILENAME -O /srv/http/images/$FILENAME
     else
       echo "Hashes of image $FILENAME match."
   fi
