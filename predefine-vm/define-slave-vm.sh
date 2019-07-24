cbe4151451fe0555d773803ffa296d2ccc0b8562
Denis Egorenko <degorenko@mirantis.com>
2019-07-24 11:52:31 +0400

    Remove not needed port for Horizon UI for AIO node OS endpoint
    Change-Id: I698f94749d30994e988e7f5f5514b8a4675219e1
    Related-Prod: TRYMCP-9

--- predefine-vm/define-slave-vm.sh
+++ predefine-vm/define-slave-vm.sh
@@ -125,5 +125,5 @@ openstackScheme=$(isoinfo -i ${VM_CONFIG_DISK} -J -x ${openstackAIOFile} | grep 
 openstackAddress=$(isoinfo -i ${VM_CONFIG_DISK} -J -x ${contextFilePath} | grep -w 'public_host' | cut -f 2 -d ':' | tr -d ' ')
 secretsFile=$(isoinfo -i ${VM_CONFIG_DISK} -J -f | grep -w "infra/secrets.yml")
 openstackPassword=$(isoinfo -i ${VM_CONFIG_DISK} -J -x ${secretsFile} | grep -E -w 'keystone_admin_password(_generated)?' | cut -f 2 -d ':' | tr -d ' ')
-echo "Once OpenStack deploy job is finished successfully OpenStack UI will be available on: ${openstackScheme}://${openstackAddress}:8078"
+echo "Once OpenStack deploy job is finished successfully OpenStack UI will be available on: ${openstackScheme}://${openstackAddress}"
 echo "Login creds are: admin / ${openstackPassword}"
